d5f21a5c40d777e48fa8478c157b624a72dd078a
Ryan Stern <ryanstern03@gmail.com>
2020-07-17 18:02:21 -0400

    refactor

--- src/api/index.js
+++ src/api/index.js
@@ -2,11 +2,8 @@ const express = require("express");
 const app = express();
 
 const weather = require("./weather/routes");
-const { consumeMsg } = require("../workers");
 
 app.use(express.json());
 app.use("/weather", weather);
-
-consumeMsg();
 
 module.exports = app;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
 const api = require("./api");
 const { PORT = 3001 } = process.env;
 
+const { consumeMsg } = require("../src/workers");
+
+// Add mood producer here
+consumeMsg();
+
 api.listen(PORT, () => console.log(`Listening on port ${PORT}`));
